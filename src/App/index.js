f607adab549f10b90c473534d06d56da014e3379
fimoefive <brothermj@hotmail.com>
2021-04-20 17:47:28 -0500

    avatar Api working and hiding the Popover button.

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import {
+  Button, Popover,
+  PopoverHeader, PopoverBody
+} from 'reactstrap';
 import './App.scss';
 import getQuestions from './helpers/data/avatarData';
 
 function App() {
+  const [popoverOpen, setPopoverOpen] = useState(false);
+  const toggle = () => setPopoverOpen(!popoverOpen);
   const [allQuestions, setAllQuestions] = useState([]);
   const [singleQuestion, setSingleQuestion] = useState({});
   const [showAnswer, setShowAnswer] = useState(false);
@@ -38,9 +44,26 @@ function App() {
       <div>
         <h1>{singleQuestion.question}</h1>
         <p>{showAnswer && singleQuestion.correctAnswer}</p>
-        <button onClick={hClick}>
+        {showAnswer ? ''
+          : <div>
+            <Button id="Popover1" type="button">
+              Launch Popover
+      </Button>
+            <Popover placement="bottom" isOpen={popoverOpen} target="Popover1" toggle={toggle}>
+              <PopoverHeader>Possible Answers</PopoverHeader>
+              <PopoverBody>
+                <ul>
+                  {singleQuestion.possibleAnsers?.map((pa, i) => <li key={i}>{pa}</li>)}
+                </ul>
+              </PopoverBody>
+              {/* <PopoverBody>{singleQuestion.possibleAnsers?.join(', ')}
+            </PopoverBody> */}
+            </Popover>
+          </div>}
+
+        <Button color="info" onClick={hClick}>
           {showAnswer ? 'Get Another Question' : 'Get Answer'}
-        </button>
+        </Button>
         <br></br>
 
         <button
